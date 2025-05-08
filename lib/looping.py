3212e970ae617a1f33de28b111bd95d376465b19
Mercy <mercymumbe17@gmail.com>
2025-05-08 11:30:12 +0300

    completed loop statements for while and loop

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    return [n ** 2 for n in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
-    pass
+     for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+        else:
+            print(n)
+        pass
